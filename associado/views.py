09f64f769107c6445026d3938cb9e150d4860356
Arvicx <arthurrodfern132@gmail.com>
2026-03-10 17:50:16 -0300

    Aula 10/03

--- associado/views.py
+++ associado/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'associado/index.html')
+
+def perfil(request):
+    return render(request, 'associado/perfil.html')
